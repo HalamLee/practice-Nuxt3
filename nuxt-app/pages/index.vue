ff12cf4148b44a9a706e6503dd80cc01e70c9f5b
Lami <Halam.Lee.Lami@gmail.com>
2023-09-22 16:21:49 +0900

    feat: useAsyncData

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/hello');
-console.log('useFetch + toRaw', toRaw(data.value));
+// useFetch + toRaw
+// const { data } = await useFetch('/api/hello');
+// console.log('useFetch + toRaw', toRaw(data.value));
 
-const { data: example } = await useFetch('/api/hello', {
-  transform: (_example) => _example.hello,
-});
+// useFetch + transform
+// const { data: example } = await useFetch('/api/hello', {
+//   transform: (_example) => _example.hello,
+// });
 
-const response = await $fetch('/api/hello');
-console.log(response);
+// $fetch
+// const response = await $fetch('/api/hello');
+// console.log(response);
 
-const { data: example2, pending } = await useLazyFetch('/api/hello', {
-  transform: (_example2) => _example2.hello,
-});
+// useLazyFetch
+// const { data: example2, pending } = await useLazyFetch('/api/hello', {
+//   transform: (_example2) => _example2.hello,
+// });
+
+// useAsyncData
+const { data: productCount, pending } = await useAsyncData('count', () =>
+  $fetch('/api/hello')
+);
+
+const refresh = () => refreshNuxtData('count');
 </script>
 
 <template>
-  <p>{{ example }}</p>
-  <p>{{ pending ? 'Loading' : example2 }}</p>
+  <!-- <p>{{ example }}</p> -->
+  <!-- <p>{{ pending ? 'Loading' : example2 }}</p> -->
+  <p>{{ pending ? 'Loading' : productCount }}</p>
+  <button @click="refresh">Refresh</button>
 </template>
--- nuxt-app/server/api/hello.get.ts
+++ nuxt-app/server/api/hello.get.ts
@@ -7,12 +7,26 @@
 // });
 
 // useLazyFetch
-export default defineEventHandler(async () => {
-  return new Promise<any>((resolve) => {
+// export default defineEventHandler(async () => {
+//   return new Promise<any>((resolve) => {
+//     setTimeout(() => {
+//       resolve({
+//         hello: 'world',
+//       });
+//     }, 2000);
+//   });
+// });
+
+// useAsyncData
+let productCount = 0;
+
+export default async () => {
+  await new Promise((resolve) => {
     setTimeout(() => {
-      resolve({
-        hello: 'world',
-      });
+      resolve(productCount++);
     }, 2000);
   });
-});
+  return {
+    productCount,
+  };
+};
