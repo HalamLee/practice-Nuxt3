822b4301fdc60fb6b67b91696affb0394de20969
Lami <Halam.Lee.Lami@gmail.com>
2023-09-25 22:15:16 +0900

    feat: title 설정

--- top-restaurants/pages/restaurants/index.vue
+++ top-restaurants/pages/restaurants/index.vue
@@ -1,4 +1,12 @@
 <template>
+  <Head>
+    <Title>Top 50 Restaurants</Title>
+    <Meta name="description" content="My page is cool" />
+    <Link
+      rel="preload"
+      href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+      as="script" />
+  </Head>
   <div>
     <NuxtLayout name="custom">
       <div class="container">
