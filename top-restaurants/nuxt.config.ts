5b0df90d5616d3ac7f29f8e80442a7027ca2178a
Lami <Halam.Lee.Lami@gmail.com>
2023-09-25 11:42:42 +0900

    chore: 부트스트랩 CDN 사용하기 위한 설정

--- top-restaurants/nuxt.config.ts
+++ top-restaurants/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+        },
+      ],
+    },
+  },
+});
